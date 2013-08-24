a4ed500c844f14d8cdb7b85e97d68d66c6010428
Rahul P Nath <rahulpnath@gmail.com>
2013-08-24 18:23:52 +0530

    Base Checkin

---
+++ Jumplist/Model/Person.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Jumplist.Model
+{
+    public class Person
+    {
+        public string Name { get; set; }
+
+    }
+}
---
+++ Jumplist/Repository/DataRepository.cs
@@ -0,0 +1,18 @@
+﻿using Jumplist.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Jumplist.Repository
+{
+    public class DataRepository
+    {
+        public static List<Person> Persons { get; internal set; }
+
+        static DataRepository()
+        {
+            Persons = new List<Person>();
+        }
+    }
+}
